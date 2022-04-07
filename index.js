26b5274bed89eca051e5e5e2dd2b8594585469ba
zhangyong <1046788379@qq.com>
2022-04-07 15:53:39 +0800

    自动爬取数据

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-var robot = require('robotjs')
+var robot = require('robotjs');
 
-const axios = require('axios')
-(async()=>{
-      const response = await  axios.get('https://sentence.iciba.com/index.php?callback=a&c=dailysentence&m=getdetail&title=2022-04-06&_=1649242108331')
-      console.log(response.data)
-    }
-)();
+const axios = require('axios');
+
+function getDate(sign='-'){
+    let dateObj  = new Date()
+    let year = dateObj.getFullYear()
+    let month = dateObj.getMonth()+1
+    month= month<=9?'0'+month:month
+    let date = dateObj.getDate()
+    date= date<=9?'0'+date:date
+    return year+sign+month+sign+date
+}
+
+let msg = ''
+;(async()=>{
+      const response = await axios.get(`https://sentence.iciba.com/index.php?callback=&c=dailysentence&m=getdetail&title=${getDate()}&_=1649242108331`)
+    
+   msg =  `机器人小智：
+
+    ${response.data.content}
+    ${response.data.note}
+    ${response.data.picture2}
+
+    ${getDate()}
+    `
+
+    // console.log(msg)
+})();
 
 
-return false
+
 var {
     exec,
 
@@ -32,7 +53,7 @@ function sendMsg(to, msg) {
                 robot.keyTap('v', 'control')
                 robot.keyTap('enter')
 
-                // robot.keyTap('d', 'command')
+                
             }, 300);
         })
     })
@@ -62,13 +83,7 @@ function start() {
 
 let nameList = ['读书分享群','媛创读书群','小uni','2011级化工群','数据结构与算法', '中国合伙人','羽毛球中南林广州','广州前端技术交流群','中南林安徽校友群','加速的朋友']
 
-let msg = `机器人小智：
 
-You can't change the world, but you can change yourself.
-你无法改变世界，但你可以改变自己。
-    
-2022年4月6日
-`
 
 // 打开微信
 robot.keyTap('o', 'control')
